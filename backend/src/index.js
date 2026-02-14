0b6637f5b8ff455e882581443dab33ad11bd9d6b
ameenhyder-v <ameenhyder.v@gmail.com>
2026-02-14 15:00:40 +0530

    cros settings changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,12 @@ const __dirname = path.resolve();
 app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ],
+    credentials: true
+  }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
